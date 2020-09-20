38b0d66c3dcb1f0699e1630b988eac55271dc706
maxfu <fxy83613@gmail.com>
2020-09-20 11:59:46 +1000

    Revert "OGSR x64 Engine Shaders"
    This reverts commit b1a9623ab90d228e96d3086770e040bec7598a92.

--- shaders/r2/effects_waterryaska.s
+++ shaders/r2/effects_waterryaska.s
@@ -3,12 +3,11 @@ local tex_nmap                = "water\\water_normal"
 local tex_dist                = "water\\water_dudv"
 local tex_env0                = "$user$sky0"         -- "sky\\sky_8_cube"
 local tex_env1                = "$user$sky1"         -- "sky\\sky_8_cube"
-local tex_leaves              = "water\\water_foam"
 
 function normal                (shader, t_base, t_second, t_detail)
-  shader:begin                ("water_soft","water_soft")
+  shader:begin                ("water","water")
         : sorting        (2, false)
-        : blend                (true,blend.srcalpha,blend.invsrcalpha)
+        : blend                (false,blend.srcalpha,blend.invsrcalpha)
               : zb                (true,false)
              : distort        (true)
               : fog                (true)
@@ -16,14 +15,10 @@ function normal                (shader, t_base, t_second, t_detail)
   shader:sampler        ("s_nmap")       :texture  (tex_nmap)
   shader:sampler        ("s_env0")       :texture  (tex_env0)   : clamp()
   shader:sampler        ("s_env1")       :texture  (tex_env1)   : clamp()
-  shader:sampler        ("s_position")       :texture  ("$user$position") : f_none ()
-
-  shader:sampler        ("s_leaves")     :texture  (tex_leaves) : wrap()	:f_anisotropic()
-
 end
 
 function l_special        (shader, t_base, t_second, t_detail)
-  shader:begin                ("waterd_soft","waterd_soft")
+  shader:begin                ("waterd","waterd")
         : sorting        (2, true)
         : blend                (true,blend.srcalpha,blend.invsrcalpha)
         : zb                (true,false)
@@ -31,5 +26,17 @@ function l_special        (shader, t_base, t_second, t_detail)
         : distort        (true)
   shader:sampler        ("s_base")       :texture  (tex_base)
   shader:sampler        ("s_distort")    :texture  (tex_dist)
-  shader:sampler        ("s_position")       :texture  ("$user$position") : f_none ()
 end
+
+--[[
+function normal                (shader, t_base, t_second, t_detail)
+  shader:begin                ("waterd","waterd")
+        : sorting        (2, true)
+        : blend                (true,blend.srcalpha,blend.invsrcalpha)
+        : zb                (true,false)
+        : fog                (false)
+        : distort        (true)
+  shader:sampler        ("s_base")       :texture  (tex_base)
+  shader:sampler        ("s_distort")    :texture  (tex_dist)
+end
+]]
--- shaders/r2/sload.h
+++ shaders/r2/sload.h
@@ -3,164 +3,99 @@
 
 #include "common.h"
 
+
 //////////////////////////////////////////////////////////////////////////////////////////
 // Bumped surface loader                //
 //////////////////////////////////////////////////////////////////////////////////////////
-struct	surface_bumped
+struct        surface_bumped
 {
-	half4	base;
-	half3	normal;
-	half	gloss;
-	half	height;
-
+        half4         	base;
+        half3         	normal;
+        half         	gloss;
+        half         	height;
+	
 };
 
-float4 tbase( float2 tc )
-{
-	return	tex2D( s_base, tc);
+float4   tbase                 (float2 tc)        {
+        return                 tex2D                (s_base,         tc);
 }
-
-#if defined(ALLOW_STEEPPARALLAX) && defined(USE_STEEPPARALLAX)
-
-static const float fParallaxStartFade = 8.0f;
-static const float fParallaxStopFade = 12.0f;
-
-void UpdateTC( inout p_bumped I)
+#if defined(USE_Sky4CE_PARALLAXOCCLUSION) && defined(PARALLAX_OCCLUSION) && !defined(USE_PARALLAX)
+surface_bumped                sload_i         ( p_bumped I)
 {
-	if (I.position.z < fParallaxStopFade)
-	{
-		const float maxSamples = 25;
-		const float minSamples = 5;
-		const float fParallaxOffset = -0.013;
+        surface_bumped      S;
+        #ifdef USE_TEXTURE_PACK
+ 		float2 vTexCoord = lod(I.eye,I.tcdh,s_base);                 
+   		float2 dTexCoords = ddx(I.tcdh) * ddx(I.tcdh) + ddy(I.tcdh) * ddy(I.tcdh);
+        if(max(dTexCoords.x,dTexCoords.y) <= 0.00007) vTexCoord = AdvancedParallax(I.eye,I.tcdh,s_base);
+        #else
+        float2 vTexCoord = lod(I.eye,I.tcdh,s_bumpX);                 
+   		float2 dTexCoords = ddx(I.tcdh) * ddx(I.tcdh) + ddy(I.tcdh) * ddy(I.tcdh);
+        if(max(dTexCoords.x,dTexCoords.y) <= 0.00007) vTexCoord = AdvancedParallax(I.eye,I.tcdh,s_bumpX);
+        #endif
+        float4       Nu     =       tex2D		(s_bump,         vTexCoord);                // IN:  normal.gloss
+        float4       NuE    =       tex2D      	(s_bumpX,       vTexCoord);                // IN:         normal_error.height
+        S.base              =       tbase                (vTexCoord);                                // IN:  rgb.a
+        S.normal            = 		Nu.wzyx + (NuE.xyz - 1.0h);							 // (Nu.wzyx - .5h) + (E-.5)
+        S.gloss             =       Nu.x*Nu.x	;                                        //        S.gloss             =        Nu.x*Nu.x;
+        S.height            =       NuE.z       ;
 
-		float3	 eye = mul (float3x3(I.M1.x, I.M2.x, I.M3.x,
-									 I.M1.y, I.M2.y, I.M3.y,
-									 I.M1.z, I.M2.z, I.M3.z), -I.position.xyz);
-		
-		eye = normalize(eye);
-		
-		//	Calculate number of steps
-		float nNumSteps = lerp( maxSamples, minSamples, eye.z );
+#ifdef		  USE_TDETAIL
+        float4       detail =		tex2D(s_detail,vTexCoord * dt_params )        	;
+        S.base.rgb          =		S.base.rgb     * detail.rgb*2		;
+        S.gloss             =  		S.gloss * detail.w * 2				;
+#endif 
 
-		float	fStepSize			= 1.0 / nNumSteps;
-		float2	vDelta				= eye.xy * fParallaxOffset*1.2;
-		float2	vTexOffsetPerStep	= fStepSize * vDelta;
-
-		//	Prepare start data for cycle
-		float2	vTexCurrentOffset	= I.tcdh;
-		float	fCurrHeight			= 0.0;
-		float	fCurrentBound		= 1.0;
-/*
-		for( int i=0; i<nNumSteps; ++i )
-		{
-			if (fCurrHeight < fCurrentBound)
-			{	
-				vTexCurrentOffset += vTexOffsetPerStep;		
-				fCurrHeight = s_bumpX.SampleLevel( smp_base, vTexCurrentOffset.xy, 0 ).a; 
-				fCurrentBound -= fStepSize;
-			}
-		}
-*/
-
-		//[unroll(25)]	//	Doesn't work with [loop]
-		for( ;fCurrHeight < fCurrentBound; fCurrentBound -= fStepSize )
-		{
-			vTexCurrentOffset += vTexOffsetPerStep;		
-			fCurrHeight = tex2Dlod( s_bumpX, float4(vTexCurrentOffset.xy,0,0) ).a; 
-		}
-
-		//	Reconstruct previouse step's data
-		vTexCurrentOffset -= vTexOffsetPerStep;
-		float fPrevHeight = tex2D( s_bumpX, float3(vTexCurrentOffset.xy,0) ).a;
-
-		//	Smooth tc position between current and previouse step
-		float	fDelta2 = ((fCurrentBound + fStepSize) - fPrevHeight);
-		float	fDelta1 = (fCurrentBound - fCurrHeight);
-		float	fParallaxAmount = (fCurrentBound * fDelta2 - (fCurrentBound + fStepSize) * fDelta1 ) / ( fDelta2 - fDelta1 );
-		float	fParallaxFade 	= smoothstep(fParallaxStopFade, fParallaxStartFade, I.position.z);
-		float2	vParallaxOffset = vDelta * ((1- fParallaxAmount )*fParallaxFade);
-		float2	vTexCoord = I.tcdh + vParallaxOffset;
-	
-		//	Output the result
-		I.tcdh = vTexCoord;
-
-#if defined(USE_TDETAIL) && defined(USE_STEEPPARALLAX)
-		I.tcdbump = vTexCoord * dt_params;
-#endif
-	}
-
+        return                S;
 }
+#else
 
-#elif	defined(USE_PARALLAX) || defined(USE_STEEPPARALLAX)
-
-void UpdateTC( inout p_bumped I)
+#ifdef         USE_PARALLAX
+surface_bumped                sload_i         ( p_bumped I)        // + texld, mad, nrm(3), mad  = 1+1+3+1 = 6, 15+6=21, OK
 {
-	float3	 eye = mul (float3x3(I.M1.x, I.M2.x, I.M3.x,
-								 I.M1.y, I.M2.y, I.M3.y,
-								 I.M1.z, I.M2.z, I.M3.z), -I.position.xyz);
-	
-	half	height	= tex2D( s_bumpX, I.tcdh).w;	//
-			//height  /= 2;
-			//height  *= 0.8;
-			height	= height*(parallax.x) + (parallax.y);	//
-	float2	new_tc  = I.tcdh + height * normalize(eye);	//
+        surface_bumped      S;
+        half        height	=       tex2D      (s_bumpX, I.tcdh).w;                                //
+                    height  =       height*(parallax.x) + (parallax.y);                        //
+        float2		new_tc  =       I.tcdh + height*normalize        (I.eye);                //
+        half4       Nu      =       tex2D		(s_bump,         new_tc);                // IN:  normal.gloss
+        half4       NuE     =       tex2D      	(s_bumpX,       new_tc);                // IN:         normal_error.height
+        S.base              =       tbase                (new_tc);                                // IN:  rgb.a
+        S.normal            = 		Nu.wzyx + (NuE.xyz - 1.0h);							 // (Nu.wzyx - .5h) + (E-.5)
+        S.gloss             =       Nu.x*Nu.x	;                                        //        S.gloss             =        Nu.x*Nu.x;
+        S.height            =       NuE.z       ;
 
-	//	Output the result
-	I.tcdh.xy = new_tc;
-}
-
-#else	//	USE_PARALLAX
-
-void UpdateTC( inout p_bumped I)
-{
-	;
-}
-
-#endif	//	USE_PARALLAX
-
-surface_bumped sload_i( p_bumped I)
-{
-	surface_bumped	S;
-
-	UpdateTC(I);	//	All kinds of parallax are applied here.
-
-	half4 	Nu	= tex2D( s_bump, I.tcdh);		// IN:	normal.gloss
-	half4 	NuE	= tex2D( s_bumpX, I.tcdh);	// IN:	normal_error.height
-
-	S.base		= tbase(I.tcdh);				//	IN:  rgb.a
-	S.normal	= Nu.wzy + (NuE.xyz - 1.0h);	//	(Nu.wzyx - .5h) + (E-.5)
-	S.gloss		= Nu.x*Nu.x;					//	S.gloss = Nu.x*Nu.x;
-	S.height	= NuE.z;
 
 #ifdef        USE_TDETAIL
-#ifdef        USE_TDETAIL_BUMP
-	half4 NDetail		= tex2D( s_detailBump, I.tcdbump);
-	half4 NDetailX		= tex2D( s_detailBumpX, I.tcdbump);
-	S.gloss				= S.gloss * NDetail.x * 2;
-	//S.normal			+= NDetail.wzy-.5;
-	S.normal			+= NDetail.wzy + NDetailX.xyz - 1.0h; //	(Nu.wzyx - .5h) + (E-.5)
-
-	half4 detail		= tex2D( s_detail, I.tcdbump);
-	S.base.rgb			= S.base.rgb * detail.rgb * 2;
-
-//	S.base.rgb			= float3(1,0,0);
-#else        //	USE_TDETAIL_BUMP
-	half4 detail		= tex2D( s_detail, I.tcdbump);
-	S.base.rgb			= S.base.rgb * detail.rgb * 2;
-	S.gloss				= S.gloss * detail.w * 2;
-#endif        //	USE_TDETAIL_BUMP
+        half4       detail  =		tex2D(s_detail,I.tcdbump)        	;
+        S.base.rgb          =		S.base.rgb     * detail.rgb*2		;
+        S.gloss             =  		S.gloss * detail.w * 2				;
 #endif
-	return S;
+	
+        return                S;
 }
+#else
+surface_bumped                sload_i         ( p_bumped I)
+{
+        surface_bumped      S;
+        half4 Nu 			=		tex2D                (s_bump, I.tcdh);                        // IN:  normal.gloss
+        half4 NuE           =		tex2D                (s_bumpX,I.tcdh);                        // IN:         normal_error.height
+        S.base              =		tbase                (I.tcdh)		;                         // IN:  rgb.a
+        S.normal            =		Nu.wzyx + (NuE.xyz - 1.0h)			;
+        S.gloss             =		Nu.x*Nu.x							;                         //        S.gloss             =        Nu.x*Nu.x;
+        S.height            = 		NuE.z;
 
-surface_bumped sload ( p_bumped I)
+#ifdef        USE_TDETAIL
+        half4 detail		=       tex2D(s_detail,I.tcdbump)    ;
+        S.base.rgb          =      	S.base.rgb*detail.rgb        	*2      ;
+        S.gloss             =  		S.gloss * detail.w * 2			;
+#endif
+        return              S;
+}
+#endif
+#endif
+surface_bumped              sload                 ( p_bumped I)
 {
         surface_bumped      S   = sload_i	(I);
 		S.normal.z			*=	0.5;		//. make bump twice as contrast (fake, remove me if possible)
-
-#if defined(ALLOW_STEEPPARALLAX) && defined(USE_STEEPPARALLAX)
-//		S.base.yz = float2(0,0);
-#endif
 
         return              S;
 }
